ca0c44932af3133030c8bea2ed2dd51cd7a03016
justttry <kerzhao@163.com>
2016-05-04 22:36:13 +0800

    first import

---
+++ src/stock_alerter/stock.py
@@ -0,0 +1,18 @@
+# encoding:UTF-8
+
+
+########################################################################
+class Stock:
+    """"""
+
+    #----------------------------------------------------------------------
+    def __init__(self, symbol):
+        """Constructor"""
+        self.symbol = symbol
+        self.price = None
+    
+    #----------------------------------------------------------------------
+    def update(self, timestamp, price):
+        if price < 0:
+            raise ValueError("price should not be negative")        
+        self.price = price
